5e6d8f0417853e115d9e91b6b75368c718bb2a53
Jessica <afraidspy@gmail.com>
2026-02-25 07:33:40 -0600

    Agregando soluciones ejercicio 01

---
+++ Ejemplos02.py
@@ -0,0 +1,76 @@
+#Python es un lenguaje de tipado dinámico
+dia = "Lunes"
+
+#str
+print(type(dia)) 
+dia = 1 #int 
+suma = dia + 1
+print(type(dia))
+dia = 'Martes' #str
+
+print(dia)
+edad = 15
+nueva_edad = edad + 1000
+edad = 30
+print(edad)
+print(nueva_edad)
+nombre = None;
+print(type(nombre))
+
+# Operaciones aitméticas
+
+"""
+a = 10
+b = 3
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+
+"""
+
+# Operadores lógicos
+"""
+a = True
+b = False
+print(a and b)
+print(a or b)
+print(not a)
+print(not (a or b))
+"""
+
+"""
+x = 10
+print(x > 5 and x < 20)
+print(x > 5 and x < 9)
+print(x > 5 or x < 9)
+"""
+
+#Operadores de asingación
+"""
+n = 10
+n += 5
+n *= 2
+n -= 4
+print(n)
+"""
+
+
+#Operadores de pertenencia
+"""
+name = "Maria"
+print("a" in name)
+print("z" in name)
+print("ri" in name)
+print("ma" in name)
+"""
+
+#Precedencia de operadores
+
+"""
+result = 2 + 3 * 4
+print(result)
+"""
