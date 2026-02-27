76491b9e6ac4e59a2fa7485ac9536771bea191c2
Jessica <afraidspy@gmail.com>
2026-02-26 21:09:00 -0600

    -Agregando ejercicio para crear un programa que convierta pesos a libras y viceversa

---
+++ ejercicios02.py
@@ -0,0 +1,13 @@
+'''
+Programa interactivo para hacer la conversión de peso a libra y de libra a peso
+author: Jessica
+date: 2026
+version 1.1
+'''
+#los kgs que vale una libra
+VALOR_LIBRAS = 0.453592
+
+#las libras que vale un kilogramo
+VALOR_KILOS = 2.20462
+
+
